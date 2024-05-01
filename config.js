ff9e6e1d0d7a8254d4b24bb16b5f8e093da8219d
Bonifacemu <154499564+Bonifacemu@users.noreply.github.com>
2024-05-01 12:51:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -24,16 +24,16 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://te
 
 global.devs = "923184474176" // Developer Contact
 global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "25471077266";
+global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "+254 101323690";
 
 
 
 
 //========================= [ BOT SETTINGS ] =========================\\
 global.style = process.env.STYLE   || '1',  // put '1' to "5" here to check bot styles
-global.flush = process.env.FLUSH   || "false"; // Make it "true" if bot not responed
-global.gdbye = process.env.GOODBYE || "false"; 
-global.wlcm  = process.env.WELCOME || "false";  // Make it "false" for disable WELCOME 
+global.flush = process.env.FLUSH   || "true"; // Make it "true" if bot not responed
+global.gdbye = process.env.GOODBYE || "true"; 
+global.wlcm  = process.env.WELCOME || "true";  // Make it "true" for disable WELCOME 
 
 global.warncount = process.env.WARN_COUNT || 3,
 global.disablepm = process.env.DISABLE_PM || "false",
@@ -44,13 +44,13 @@ global.waPresence= process.env.WAPRESENCE ||  "set according to your need" ; // 
 
 //========================= [ AUTO READ MSGS & CMDS ] =========================\\
 global.readcmds = process.env.READ_COMMAND || "false"
-global.readmessage = process.env.READ_MESSAGE || "false"
+global.readmessage = process.env.READ_MESSAGE || "true"
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "25471077266,254xxxxxxxx";
 
 
 //========================= [ AUTO SAVE & READ STATUS ] =========================\\
 global.read_status = process.env.AUTO_READ_STATUS || "true"
-global.save_status = process.env.AUTO_SAVE_STATUS || "false"
+global.save_status = process.env.AUTO_SAVE_STATUS || "true"
 global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923xxxxxxxx";
 global.read_status_from =  process.env.READ_STATUS_FROM  ||  "254710772666,254xxxxxxxx";
 
@@ -69,9 +69,9 @@ module.exports = {
   author : process.env.PACK_AUTHER|| "Ibrahim",
   packname: process.env.PACK_NAME || "👑",
   botname : process.env.BOT_NAME  || " 𝗜𝗕𝗥𝗔𝗛𝗜𝗠-ᵐᵈ",
-  ownername:process.env.OWNER_NAME|| "It'x 𝗜𝗕𝗥𝗔𝗛𝗜𝗠",
+  ownername:process.env.OWNER_NAME|| "𝔫𝔦𝔱𝔠𝔥𝔨𝔦𝔡𝔬𝔣𝔣𝔦𝔠𝔦𝔞𝔩",
 
-  sessionName:process.env.SESSION_ID || "",  // PUT SESSION ID HERE 
+  sessionName:process.env.SESSION_ID || "PANTHER;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoia0ZqaE1wZk13SEtYYU9IOUFNeFB0Y3RCOXhvcFpDazlESmFpcnlaZ2ptYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibXdqWkQ5eDduMTViSXh3RCtBUUx4bnJzLzZxalp4eURIcFAwWmdqb3R6az0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJvS0plbzU2Um4yNE52U1A3K2FqeFFwRW4wTktPN2p6aGErSWtyeFlwN1c4PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJIRThoanFiYnlXZEtleXk1K2dEYnRkbDhHbENuWDFWcEZFWXBsMGF1cGxJPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IitDTkxSc0pBMDdxUFhTTXZLVytrYmNIMmMzbnJoaEpoMDlTMllac2xBM1k9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjNlY1Vsb1ZYWjM4VjhwVzFkMGh1bllIWlhNeVdzV1RjaStaSWZsOVFtaU09In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiME9GNUpIVkEvK2hYZ1B3Z0JIeS9ubU5NRWhZRFB1ZCthM0lZbHZsaTRuST0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiS1BHWU9PclRpMGY4VGkwZkthaUR3K2hsUUxwVDJpeGFyMVRKcGovTjFrOD0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IllxYzM0SitIdFRvUjZVV0YvL1VwMk0xaTBsOGhndXkzTFhPc2x1RGtvK1ZEYXpsWVAxOFk5aTE4MDBYd2w3UUFvT1JZS2FPYTBJbmlSRHB2MDVZTGpRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTAyLCJhZHZTZWNyZXRLZXkiOiJaS0VSeGZSa1JTczJKanNmSlpDRUtHcCs3K2tkdDMxZFlWNU5wTEVIeHBjPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJabWRNRW10ZVNZbXRkcFlyR1JnV0x3IiwicGhvbmVJZCI6ImYwMTc1ZGFhLTE4MGYtNGRiMS1iYTdlLWQzMDQ2YzQ2MzhlNiIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJmVmMzTk1hSk1Jd1plOGo2c0Q0QjRhcjQzYTA9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMTU4SWRvTDgrVkl5NDI5LzI5V1RKN0FMaFEwPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IkpaOTlQM1JEIiwibWUiOnsiaWQiOiIyNTQxMDEzMjM2OTA6OUBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDSlNXak04Q0VLS2R5TEVHR0FNZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiNHdPc0g0aEcxNVI4TVRIdWZ3Z0RqOW41M0NPTDd2NTBoVDVrZUdIMEUyUT0iLCJhY2NvdW50U2lnbmF0dXJlIjoiRE1wZHVjbnFkaDNMOGJpeDl4S1hRWmQ5RjVyME9GOER1TlV2U3dwcUllVUF1eWE2U2Nnc3g0dUNNd0lwRlVLVlpxTGFSMGFwMkxHTGtMMGRBN1puQmc9PSIsImRldmljZVNpZ25hdHVyZSI6ImdKYytCRGt5bkpDN1BaV1RiaXBteXRHeC9VdGtyMnZ4V2VUZXZoeEdtQnhQVCtRb0hCbGhOd2JNc1VZYTVEdjFsMHBlSlZUV093YjBVN2xmNk1NY2hBPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjU0MTAxMzIzNjkwOjlAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCZU1EckIrSVJ0ZVVmREV4N244SUE0L1orZHdqaSs3K2RJVStaSGhoOUJOayJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcxNDU1NjU5MX0=",  // PUT SESSION ID HERE 
   errorChat : process.env.ERROR_CHAT || "",
   KOYEB_API : process.env.KOYEB_API  || "false",
 
@@ -84,7 +84,7 @@ module.exports = {
 
   aitts_Voice_Id : process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE||process.env.MODE || "private",
+  WORKTYPE: process.env.WORKTYPE||process.env.MODE || "public",
   LANG: process.env.THEME ? process.env.THEME.toUpperCase() : "ADAM",
 
 
@@ -125,13 +125,13 @@ fs.watchFile(file, () => { fs.unwatchFile(file);console.log(`Update'${__filename
 
 // ========================= [ Disables in V.1.2.8 ] ===============================\\  
   //style : process.env.STYLE || "2",  // put '1' & "2" here to check bot styles
-  //readmessage:process.env.READ_MESSAGE|| "false",
+  //readmessage:process.env.READ_MESSAGE|| "true",
   //warncount: process.env.WARN_COUNT || 3,
   //userImages:process.env.USER_IMAGES|| "text",  // SET IMAGE AND VIDEO URL FOR BOT MENUS 
   //disablepm: process.env.DISABLE_PM || "false",
   //MsgsInLog: process.env.MSGS_IN_LOG|| "false", // "true"  to see messages , "log" to open logs , "false" to hide logs messages
   //readcmds:process.env.READ_COMMANDS|| "false", 
   //alwaysonline:process.env.WAPRESENCE|| "unavailable", // 'unavailable' | 'online' | 'composing' | 'recording' | 'paused'
-  //read_status: process.env.AUTO_READ_STATUS || "false",
-  //save_status: process.env.AUTO_SAVE_STATUS || "false",
+  //read_status: process.env.AUTO_READ_STATUS || "true",
+  //save_status: process.env.AUTO_SAVE_STATUS || "ture",
  
